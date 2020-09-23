e90192eec570963076f267a1e9240b478b2527f6
Pierre-Julien Villoud <pjvilloud@protonmail.com>
2020-09-23 12:00:43 +0200

    Modifs en profondeur

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
     });
   });
 
-  
+
 });
 var nbSlides = $(".step.slide").length;
 var showPopover = new URL(window.location).searchParams.get("showPopover");
@@ -19,7 +19,7 @@ var myTemplateConfig = {
       lineWidth: 10,
       spacingX: 50,
       labelRotation: 0,
-      showLabel: true,   
+      showLabel: true,
     },
     commit: {
       spacingY: -60,
@@ -52,38 +52,119 @@ var master = graphLocal.branch("master");
 var masterRemote = graphRemote.branch("origin/master");
 var fixVilloud;
 var noms = ['girard', 'durand', 'faure'];
-$("#gitPull").click(function(event) {
-    master.commit("Création du fichier villoud.txt");
-    for (var i = 0; i < noms.length; i++) {
-      master.commit("Création du fichier " + noms[i] + ".txt");
-    }
+
+$("#gitCreate").click(function(event) {
+    masterRemote.commit({
+      message: "Initial commit",
+      sha1: "acf091c"
+    });
+    $("#gitCreate").removeClass("btn-primary").addClass("btn-success");
 });
 $("#gitClone").click(function(event) {
-    master.commit("Initial commit");
-    masterRemote.commit("Initial commit");
+    master.commit({
+      message: "Initial commit",
+      sha1: "acf091c"
+    });
+    $("#gitClone").removeClass("btn-primary").addClass("btn-success");
 });
 $("#masterlocal1stcommit").click(function(event) {
-    master.commit("Création du fichier villoud.txt");
+    master.commit({
+      message: "Création du fichier villoud.txt",
+      sha1: "5ddb756"
+    });
+    $("#masterlocal1stcommit").removeClass("btn-primary").addClass("btn-success");
+
 });
 $("#masterremote1stpush").click(function(event) {
-    masterRemote.commit("Création du fichier villoud.txt");
-});
-$("#masterremote2ndpush").click(function(event) {
-    graphRemote = new GitGraph({
-      template: myTemplate,
-      reverseArrow: false,
-      orientation: "vertical",
-      elementId: "gitGraphRemote",
-      mode: "compact"
+    masterRemote.commit({
+      message: "Création du fichier villoud.txt",
+      sha1: "5ddb756"
     });
-    masterRemote = graphRemote.branch("origin/master");
-    masterRemote.commit("Initial commit");
-    masterRemote.commit("Création du fichier dupond.txt");
-    masterRemote.commit("Création du fichier villoud.txt");
-    for (var i = 0; i < noms.length; i++) {
-            masterRemote.commit("Création du fichier " + noms[i] + ".txt");
-    }
+    $("#masterremote1stpush").removeClass("btn-primary").addClass("btn-success");
 });
+
+$("#masterteamcommitlocal").click(function(event) {
+  graphLocal = new GitGraph({
+    template: myTemplate,
+    reverseArrow: false,
+    orientation: "vertical",
+    elementId: "gitGraphLocal",
+    mode: "compact"
+  });
+  master = graphLocal.branch("master");
+  master.commit({
+    message: "Initial commit",
+    sha1: "acf091c"
+  });
+  master.commit({
+    message: "Création du fichier dupond.txt",
+    sha1: "5ea96de"
+  });
+  for (var i = 0; i < noms.length; i++) {
+    (function(index) {
+        setTimeout(function() {
+          masterRemote.commit("Création du fichier " + noms[index] + ".txt");
+        }, i * 2000);
+    })(i);
+  }
+  $("#masterteamcommitlocal").removeClass("btn-primary").addClass("btn-success");
+
+});
+
+$("#masterteampusherror").click(function(event) {
+  $("#list-push").addClass("list-group-item-danger");
+  $("#masterteampusherror").removeClass("btn-primary").addClass("btn-danger");
+  $("#masterteampusherror i").removeClass("fa-check").addClass("fa-times");
+
+});
+
+$("#masterteampushsuccess").click(function(event) {
+  graphRemote = new GitGraph({
+    template: myTemplate,
+    reverseArrow: false,
+    orientation: "vertical",
+    elementId: "gitGraphRemote",
+    mode: "compact"
+  });
+  masterRemote = graphRemote.branch("origin/master");
+  masterRemote.commit({
+    message: "Initial commit",
+    sha1: "acf091c"
+  });
+  masterRemote.commit({
+    message: "Création du fichier dupond.txt",
+    sha1: "5ea96de"
+  });
+  for (var i = 0; i < noms.length; i++) {
+    masterRemote.commit({
+      message: "Création du fichier " + noms[i] + ".txt",
+      sha1: "1de946" + i
+    });
+  }
+  masterRemote.commit({
+    "message": "Merge branch 'master' of github.com:pjvilloud/ipi-git-200-ex",
+    "sha1": "f85c456"
+  })
+  $("#list-push2").addClass("list-group-item-success");
+  $("#masterteampushsuccess").removeClass("btn-primary").addClass("btn-success");
+
+});
+
+$("#gitpull2").click(function(event) {
+    for (var i = 0; i < noms.length; i++) {
+      master.commit({
+        message: "Création du fichier " + noms[i] + ".txt",
+        sha1: "1de946" + i
+      });
+    }
+    master.commit({
+      "message": "Merge branch 'master' of github.com:pjvilloud/ipi-git-200-ex",
+      "sha1": "f85c456"
+    })
+    $("#gitpull2").removeClass("btn-primary").addClass("btn-success");
+
+});
+
 $("#createfixvotrenombranch").click(function(event) {
     fixVilloud = master.branch("fix-villoud");
 });
@@ -96,7 +177,7 @@ $("#commitRemoveNomTxt").click(function(event) {
 $("#commitIntermediaireMerge").click(function(event) {
     master.commit("Modification mineure");
     setTimeout(function() { master.merge(fixVilloud);}, 2000);
-    
+
 });
 
 $("#mergeFixNomTxtMaster").click(function(event) {
@@ -108,20 +189,20 @@ $("#deleteFixNomTxt").click(function(event) {
     var branches = [];
     for (var i = 0; i < noms.length; i++) {
       (function(index) {
-          setTimeout(function() { 
+          setTimeout(function() {
             branches[index] = masterRemote.branch("fix-" + noms[index]);
             branches[index].commit("Suppression de " + noms[index] + ".txt");
           }, i * 2000);
       })(i);
     }
-    setTimeout(function() { 
+    setTimeout(function() {
       branches[0].merge(branches[1]);
-      
+
     }, 8000);
-    setTimeout(function() { 
+    setTimeout(function() {
       branches[1].merge(branches[2]);
     }, 10000);
-    setTimeout(function() { 
+    setTimeout(function() {
       branches[2].merge(masterRemote);
     }, 12000);
 });
@@ -145,7 +226,6 @@ rootElement.addEventListener( "impress:stepenter", function(event) {
       container: 'body'
     });
   }
-  
   if($(currentStep).attr("id") === "changements-pull"){
     graphLocal = new GitGraph({
       template: myTemplate,
@@ -155,15 +235,24 @@ rootElement.addEventListener( "impress:stepenter", function(event) {
       mode: "compact"
     });
     master = graphLocal.branch("master");
-    master.commit("Initial commit");
-    master.commit("Création du fichier dupond.txt");
-    for (var i = 0; i < noms.length; i++) {
-      (function(index) {
-          setTimeout(function() { 
-            masterRemote.commit("Création du fichier " + noms[index] + ".txt");
-          }, i * 2000);
-      })(i);
-    }
+    master.commit({
+      message: "Initial commit",
+      sha1: "acf091c"
+    });
+
+    graphRemote = new GitGraph({
+      template: myTemplate,
+      reverseArrow: false,
+      orientation: "vertical",
+      elementId: "gitGraphRemote",
+      mode: "compact"
+    });
+    masterRemote = graphRemote.branch("master");
+    masterRemote.commit({
+      message: "Initial commit",
+      sha1: "acf091c"
+    });
+
   }
 
 });
@@ -192,7 +281,7 @@ $(window).on('hashchange', function(e){
       $('#my-navbar li a[href="' + newHash.substring(0,newHash.lastIndexOf("-")) + '"]').addClass("active");
     }
     $('#my-navbar li a[href="' + newHash + '"]').addClass("active");
-    
+
 });
 
 //-------------------
@@ -204,7 +293,7 @@ $("div.step.slide").each(function(index, el) {
   var yOffset = 1100;
   var xOffset = 2000;
   var ybase = 1100;
-  var xbase = 0; 
+  var xbase = 0;
   if(id !== 'accueil'){
     if(id.indexOf("-") > 0){
       if(id.lastIndexOf("-") != id.indexOf("-")){
@@ -229,12 +318,12 @@ $("div.step.slide").each(function(index, el) {
       }
     } else {
       if($('div[id^="'+id+'-"]').length > 0){
-        $("ul.nav.nav-pills.mr-auto").append('<li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" id="navbarDropdown'+id+'" href="#'+id+'" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">'+title+'</a> <div id="dropdown-'+id+'" class="dropdown-menu" aria-labelledby="navbarDropdown'+id+'"></div></li>');  
+        $("ul.nav.nav-pills.mr-auto").append('<li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" id="navbarDropdown'+id+'" href="#'+id+'" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">'+title+'</a> <div id="dropdown-'+id+'" class="dropdown-menu" aria-labelledby="navbarDropdown'+id+'"></div></li>');
         $(el).attr("data-x", 0);
         $(el).attr("data-rel-y", yOffset);
         $(el).attr("data-z", 0);
       } else {
-        $("ul.nav.nav-pills.mr-auto").append('<li class="nav-item"><a class="nav-link" href="#'+id+'">'+title+'</a></li>');  
+        $("ul.nav.nav-pills.mr-auto").append('<li class="nav-item"><a class="nav-link" href="#'+id+'">'+title+'</a></li>');
         $(el).attr("data-x", 0);
         $(el).attr("data-rel-y", yOffset);
         $(el).attr("data-z", 0);
